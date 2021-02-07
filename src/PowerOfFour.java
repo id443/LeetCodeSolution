060fa99f4ab653e1bb50e2237e9bac9402d125ea
Maxim <mksmsmirnov@gmail.com>
2021-02-07 09:33:31 +0300

    maximumTime

--- src/PowerOfFour.java
+++ src/PowerOfFour.java
@@ -8,5 +8,15 @@ public class PowerOfFour {
         }
         return n == 1;
     }
+    public String maximumTime(String time) {
+        char[] arrTime = time.toCharArray();
+
+        if (arrTime[0] == '?') arrTime[0] = arrTime[1] <= '3' || arrTime[1] == '?' ? '2':'1';
+        if (arrTime[1] == '?') arrTime[1] = arrTime[0]=='2' ? '3':'9';
+        if (arrTime[3] == '?') arrTime[3] = '5';
+        if (arrTime[4] == '?') arrTime[4] = '9';
+
+        return String.valueOf(arrTime);
+    }
 }
 
